e533eab027dc7488dd3e94cc28f3e867bb7618f8
Moti Zilberman <moti@meta.com>
2023-09-28 12:17:43 -0400

    Scaffold feature detail page

--- app/features/[id]/page.tsx
+++ app/features/[id]/page.tsx
@@ -1,3 +1,32 @@
-export default function Page({ params }: { params: { id: string } }) {
-  return <div>Feature id: {params.id}</div>;
+import prisma from "lib/prisma";
+
+import { Prisma } from "@prisma/client";
+
+async function getFeature({ id }: { id: string }) {
+  const feature = await prisma.feature.findUniqueOrThrow({
+    where: { id: id },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return feature;
+}
+
+type FeatureWithDetails = Prisma.PromiseReturnType<typeof getFeature>;
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const feature: FeatureWithDetails = await getFeature({ id: params.id });
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">{feature.title}</h1>
+      <p className="text-sm">Created: {feature.createdAt.toISOString()}</p>
+      <p className="text-sm">Updated: {feature.updatedAt.toISOString()}</p>
+      {feature.author != null ? (
+        <p className="text-sm">Author: {feature.author.name}</p>
+      ) : null}
+      <p>{feature.content}</p>
+    </main>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import prisma from "../lib/prisma";
+import prisma from "lib/prisma";
 import FeatureList from "./featurelist";
 
 import { Prisma } from "@prisma/client";
