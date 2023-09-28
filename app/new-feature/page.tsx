0a1c533f6181b9bbfc9832f19ce11137c90bbe1e
Alex Hunt <hello@alexhunt.io>
2023-09-28 14:46:08 -0400

    Style new feature form

--- app/new-feature/page.tsx
+++ app/new-feature/page.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import type { PropsWithChildren } from "react";
+
 import { createFeature } from "./actions";
 // @ts-expect-error experimental_useFormState is missing from types
 import { experimental_useFormState as useFormState } from "react-dom";
+import { H2 } from "ui/components/MDX/Heading";
 import { SubmitButton } from "./submit-button";
+
+function MaxWidth({ children }: PropsWithChildren) {
+  return <div className="max-w-4xl ms-0 2xl:mx-auto">{children}</div>;
+}
+
+const labelStyles =
+  "block mb-2 text-md font-medium text-gray-900 dark:text-white";
+const inputStyles =
+  "block w-full p-4 text-gray-900 border border-gray-300 rounded-lg sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
 
 const initialState = {
   message: null,
@@ -13,18 +25,33 @@ export default function Page() {
   const [state, formAction] = useFormState(createFeature, initialState);
 
   return (
-    <>
-      <div>Add a feature!</div>
-      <form action={formAction}>
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" name="title" required />
-        <label htmlFor="content">Content</label>
-        <input type="text" id="content" name="content" required />
-        <SubmitButton />
+    <MaxWidth>
+      <H2>Add a feature!</H2>
+      <form className="space-y-4" action={formAction}>
+        <label className={labelStyles} htmlFor="title">
+          Title
+        </label>
+        <input
+          className={inputStyles}
+          type="text"
+          id="title"
+          name="title"
+          required
+        />
+        <label className={labelStyles} htmlFor="content">
+          Content
+        </label>
+        <textarea
+          className={inputStyles}
+          id="content"
+          name="content"
+          required
+        />
+        <SubmitButton size="lg" />
         <p aria-live="polite" className="sr-only">
           {state?.message}
         </p>
       </form>
-    </>
+    </MaxWidth>
   );
 }
--- app/new-feature/submit-button.tsx
+++ app/new-feature/submit-button.tsx
@@ -1,12 +1,35 @@
 "use client";
 
+import classnames from "classnames";
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
-export function SubmitButton() {
+type Props = React.AnchorHTMLAttributes<HTMLButtonElement> & {
+  type?: "primary" | "secondary";
+  size?: "md" | "lg";
+};
+
+export function SubmitButton({
+  type = "primary",
+  size = "md",
+  className,
+  ...props
+}: Props) {
   const { pending } = useFormStatus();
 
+  const classes = classnames(
+    className,
+    "active:scale-[.98] transition-transform inline-flex font-bold items-center outline-none focus:outline-none focus-visible:outline focus-visible:outline-link focus:outline-offset-2 focus-visible:dark:focus:outline-link-dark leading-snug",
+    {
+      "bg-link text-white hover:bg-opacity-80": type === "primary",
+      "text-primary dark:text-primary-dark shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10 hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10":
+        type === "secondary",
+      "text-lg py-3 rounded-full px-4 sm:px-6": size === "lg",
+      "text-base rounded-full px-4 py-2": size === "md",
+    },
+  );
+
   return (
-    <button type="submit" aria-disabled={pending}>
+    <button {...props} className={classes} aria-disabled={pending}>
       Add
     </button>
   );
